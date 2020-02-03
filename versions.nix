6ba5a2a54a2095feda62a20c28564a55e13a3c5a
Quyse Lert <quyse0@gmail.com>
2020-02-03 23:13:43 +0300

    more versions to build

--- versions.nix
+++ versions.nix
@@ -1,18 +1,25 @@
 # order matters: last item in every list becomes "latest" tag
 rec {
   bitcoin = [
+    "0.18.0"
+    "0.18.1"
     "0.19.0.1"
   ];
   bitcoin-abc = [
+    "0.20.9"
+    "0.20.10"
     "0.20.11"
   ];
   bitcoin-gold = [
     "0.15.2"
   ];
   bitcoin-sv = [
+    "1.0.0"
     "1.0.1"
   ];
   cardano = [
+    "3.0.3"
+    "3.1.0"
     "3.2.0"
   ];
   cardano-fork = cardano;

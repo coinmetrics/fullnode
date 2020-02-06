08c3e93a59199759ffff6f492d4dabe0e5b907a9
Quyse Lert <quyse0@gmail.com>
2020-02-07 00:05:45 +0300

    add ethereum-parity 2.7.2

--- fullnodes/ethereum-parity.nix
+++ fullnodes/ethereum-parity.nix
@@ -6,15 +6,18 @@ rec {
     cargoSha256 = {
       "2.5.13" = "16nf6y0hyffwdhxn1w4ms4zycs5lkzir8sj6c2lgsabig057hb6z";
       "2.6.8" = "1xqmnirx2r91q5gy1skxl0f79xvaqzimq3l0cj4xvfms7mpdfbg1";
+      "2.7.2" = "164sv7xs8qgpxf978dqb343w607y6dlbc2bzn1ma45d0pz0k0n7k";
     }.${version};
     src = builtins.fetchGit {
       url = "https://github.com/paritytech/parity-ethereum.git";
       ref = "refs/tags/v${version}";
     };
 
-    nativeBuildInputs = [ cmake ];
+    nativeBuildInputs = [ cmake llvmPackages.llvm llvmPackages.clang ];
 
-    buildInputs = [ systemd ];
+    buildInputs = [ systemd llvmPackages.libclang ];
+
+    LIBCLANG_PATH = "${llvmPackages.libclang}/lib";
 
     cargoBuildFlags = [ "--features final" ];
 

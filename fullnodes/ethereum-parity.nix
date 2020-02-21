72eebf06c3cb3d811be70628aed47c6deb9bcbec
Quyse Lert <quyse0@gmail.com>
2020-02-21 19:07:33 +0300

    update 2.7.2 parity hash

--- fullnodes/ethereum-parity.nix
+++ fullnodes/ethereum-parity.nix
@@ -6,7 +6,7 @@ rec {
     cargoSha256 = {
       "2.5.13" = "0v942qap7pbmwnhra58xjyynws5b3rknv85mri53db7wha6c6ng7";
       "2.6.8" = "1kxvwi63v3rilavm29y8xz1hyg7xpd502p57260zdfx8055xfibx";
-      "2.7.2" = "164sv7xs8qgpxf978dqb343w607y6dlbc2bzn1ma45d0pz0k0n7k";
+      "2.7.2" = "04633m5kcqqadbzgqc30c1ww7y5hbfzyzfn3gxp8r6g6dih2x76m";
     }.${version};
     src = builtins.fetchGit {
       url = "https://github.com/paritytech/parity-ethereum.git";

92bcac5a90d958728ab1e94234b9a7d22d4a55c3
Quyse Lert <quyse0@gmail.com>
2020-02-21 18:49:54 +0300

    update 2.6.8 parity hash

--- fullnodes/ethereum-parity.nix
+++ fullnodes/ethereum-parity.nix
@@ -5,7 +5,7 @@ rec {
     inherit version;
     cargoSha256 = {
       "2.5.13" = "16nf6y0hyffwdhxn1w4ms4zycs5lkzir8sj6c2lgsabig057hb6z";
-      "2.6.8" = "1xqmnirx2r91q5gy1skxl0f79xvaqzimq3l0cj4xvfms7mpdfbg1";
+      "2.6.8" = "1kxvwi63v3rilavm29y8xz1hyg7xpd502p57260zdfx8055xfibx";
       "2.7.2" = "164sv7xs8qgpxf978dqb343w607y6dlbc2bzn1ma45d0pz0k0n7k";
     }.${version};
     src = builtins.fetchGit {

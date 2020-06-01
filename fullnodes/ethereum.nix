109715f3830b4aaca3f55dc065caa06466863dd9
Quyse Lert <quyse0@gmail.com>
2020-06-01 20:53:34 +0300

    ethereum 3.0.1

--- fullnodes/ethereum.nix
+++ fullnodes/ethereum.nix
@@ -8,11 +8,16 @@ rec {
       "2.6.8" = "0l89fa44rkphjibaiglvgcsk6n9gqhzf3sqs0af78jxw9ya8pchm";
       "2.7.2" = "1mfi8rh6gbdgq7gdxwdpnr53dd5cxavmy8w12b9nvbxc8lpwf832";
       "3.0.0" = "1y5kq1kmzlrax02x40wiyq4g714jl8vbfgyzaya7mybci7qy0cz2";
-    }.${version};
+      "3.0.1" = "1pzrz9hppg0b1bry1r42xi2qsn5crb4pz7alc1r5x8lzk2pif0bk";
+    }.${version} or (builtins.trace "Ethereum fullnode: using dummy cargo SHA256" "0000000000000000000000000000000000000000000000000000");
     src = builtins.fetchGit {
       url = "https://github.com/openethereum/openethereum.git";
       ref = "refs/tags/v${version}";
     };
+
+    cargoPatches = {
+      "3.0.1" = [./ethereum/3.0.1.patch];
+    }.${version} or [];
 
     nativeBuildInputs = [ cmake llvmPackages.llvm llvmPackages.clang ];
 

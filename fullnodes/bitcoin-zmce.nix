3ad56ae016c75abef8451cf1d1adf4413869d9a3
0xb10c <0xb10c@gmail.com>
2020-06-09 18:05:26 +0200

    add: Bitcoin Core ZCME v0.20.0

--- fullnodes/bitcoin-zmce.nix
+++ fullnodes/bitcoin-zmce.nix
@@ -5,9 +5,10 @@ rec {
     inherit version;
 
     src = builtins.fetchTarball "https://bitcoincore.org/bin/bitcoin-core-${version}/bitcoin-${version}.tar.gz";
-	
+
     patches = {
       "0.19.1" = [ ./bitcoin-zmce/v0.19.1-zmq-mempool-chain-events.patch ];
+      "0.20.0" = [ ./bitcoin-zmce/v0.20.0-zmce.patch ];
     }.${version};
 
     nativeBuildInputs = [ pkgconfig autoreconfHook ];

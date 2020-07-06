4e999fc495e5b5346bdb614fc51035fb049a9fc8
Alexander Bich <quyse0@gmail.com>
2020-07-06 06:21:45 +0000

    monero 0.16.0.1

--- fullnodes/monero.nix
+++ fullnodes/monero.nix
@@ -11,6 +11,7 @@ rec {
       sha256 = {
         "0.15.0.5" = "06zzwa0y8ic6x3y2fy501788r51p4klanyvmm76ywrwf087njlkv";
         "0.16.0.0" = "0x74h5z0nxxxip97ibc854pqmrgd8r4d6w62m424f66i8gbzfskh";
+        "0.16.0.1" = "0n2cviqm8radpynx70fc0819k1xknjc58cvb4whlc49ilyvh8ky6";
       }.${version};
     };
 

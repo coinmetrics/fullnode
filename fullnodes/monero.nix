aed0bfaf8c74bfbad4ca70c2be3d767686903e5d
Quyse Lert <quyse0@gmail.com>
2020-08-05 00:27:17 +0300

    dummy hash for monero

--- fullnodes/monero.nix
+++ fullnodes/monero.nix
@@ -12,7 +12,7 @@ rec {
         "0.15.0.5" = "06zzwa0y8ic6x3y2fy501788r51p4klanyvmm76ywrwf087njlkv";
         "0.16.0.0" = "0x74h5z0nxxxip97ibc854pqmrgd8r4d6w62m424f66i8gbzfskh";
         "0.16.0.1" = "0n2cviqm8radpynx70fc0819k1xknjc58cvb4whlc49ilyvh8ky6";
-      }.${version};
+      }.${version} or (builtins.trace "Monero fullnode: using dummy SHA256" "0000000000000000000000000000000000000000000000000000");
     };
 
     nativeBuildInputs = [ pkgconfig cmake ];

ea5282c47d93521c62d1c773cc49eb6a3c9d2bdf
Quyse Lert <quyse0@gmail.com>
2020-08-25 17:43:42 +0300

    geth 1.9.20

--- fullnodes/geth.nix
+++ fullnodes/geth.nix
@@ -5,6 +5,7 @@ rec {
     inherit version;
     vendorSha256 = {
       "1.9.19" = "1744df059bjksvih4653nnvb4kb1xvzdhypd0nnz36m1wrihqssv";
+      "1.9.20" = "1744df059bjksvih4653nnvb4kb1xvzdhypd0nnz36m1wrihqssv";
     }.${version} or (builtins.trace "Geth fullnode: using dummy vendor SHA256" "0000000000000000000000000000000000000000000000000000");
     src = builtins.fetchGit {
       url = "https://github.com/ethereum/go-ethereum.git";

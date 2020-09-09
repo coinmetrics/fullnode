c5dbd86c1a7354e318daf477b9db0d498b26d64e
Quyse Lert <quyse0@gmail.com>
2020-09-09 13:29:13 +0300

    coregeth 1.11.13

--- fullnodes/coregeth.nix
+++ fullnodes/coregeth.nix
@@ -9,6 +9,7 @@ rec {
       "1.11.10" = "1b6pbrlcxnj9284ia30m8x5cs6fpxbycs2dzq8p9qgar5ga9kamb";
       "1.11.11" = "1b6pbrlcxnj9284ia30m8x5cs6fpxbycs2dzq8p9qgar5ga9kamb";
       "1.11.12" = "1b6pbrlcxnj9284ia30m8x5cs6fpxbycs2dzq8p9qgar5ga9kamb";
+      "1.11.13" = "0yaly8bhk0jysmpv7bw7fvazgjsd9lia66679y2w3p9k3xb2l915";
     }.${version} or (builtins.trace "CoreGeth fullnode: using dummy vendor SHA256" "0000000000000000000000000000000000000000000000000000");
 
     src = builtins.fetchGit {

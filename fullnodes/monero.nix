ba6f8b146c2cdb514cfc4fa0913452053c532370
Quyse Lert <quyse0@gmail.com>
2020-09-16 19:14:40 +0300

    monero 0.17.0.0

--- fullnodes/monero.nix
+++ fullnodes/monero.nix
@@ -13,6 +13,7 @@ rec {
         "0.16.0.0" = "0x74h5z0nxxxip97ibc854pqmrgd8r4d6w62m424f66i8gbzfskh";
         "0.16.0.1" = "0n2cviqm8radpynx70fc0819k1xknjc58cvb4whlc49ilyvh8ky6";
         "0.16.0.3" = "1r9x3712vhb24dxxirfiwj5f9x0h4m7x0ngiiavf5983dfdlgz33";
+        "0.17.0.0" = "1mpxfpbfr6gv1acw8wyxfk5i1i3p63lm8gi88xhb42s1vakvp7ad";
       }.${version} or (builtins.trace "Monero fullnode: using dummy SHA256" "0000000000000000000000000000000000000000000000000000");
     };
 

88db94d3fdb557f874d64b290094bfe7cb7ab3f8
Quyse Lert <quyse0@gmail.com>
2020-10-15 13:53:41 +0300

    geth 1.9.23

--- fullnodes/geth/default.nix
+++ fullnodes/geth/default.nix
@@ -9,6 +9,7 @@ rec {
       "1.9.20" = "1744df059bjksvih4653nnvb4kb1xvzdhypd0nnz36m1wrihqssv";
       "1.9.21" = "155hmny3543h02ryn1nnlpmvs0qvhd0lb66vmkhw5351m6gkbx7x";
       "1.9.22" = "1qbg44cryiv9kvcak6qjrbmkc9bxyk5fybj62vdkskqfjvv86068";
+      "1.9.23" = "1qbg44cryiv9kvcak6qjrbmkc9bxyk5fybj62vdkskqfjvv86068";
     }.${version} or (builtins.trace "Geth fullnode: using dummy vendor SHA256" "0000000000000000000000000000000000000000000000000000");
 
     src = builtins.fetchGit {

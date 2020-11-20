34351b95ead551220995afa1e7a5a439b3114e54
Alexander Bich <quyse0@gmail.com>
2020-11-20 23:34:46 +0300

    coregeth 1.11.19

--- fullnodes/coregeth/default.nix
+++ fullnodes/coregeth/default.nix
@@ -15,6 +15,7 @@ rec {
       "1.11.16" = "059zm6w0v7khaqslfjnfwsb908f6xz999yknhr1ihx1wyzirkkp3";
       "1.11.17" = "059zm6w0v7khaqslfjnfwsb908f6xz999yknhr1ihx1wyzirkkp3";
       "1.11.18" = "059zm6w0v7khaqslfjnfwsb908f6xz999yknhr1ihx1wyzirkkp3";
+      "1.11.19" = "059zm6w0v7khaqslfjnfwsb908f6xz999yknhr1ihx1wyzirkkp3";
     }.${version} or (builtins.trace "CoreGeth fullnode: using dummy vendor SHA256" "0000000000000000000000000000000000000000000000000000");
 
     src = builtins.fetchGit {

d630f6b42b29f4e8d7e0cd10596a3fea128e0ba6
Alexander Bich <quyse0@gmail.com>
2020-11-24 17:15:28 +0300

    Revert "cosmos-rosetta 0.2.0": it is library now, wtf
    This reverts commit d90459d3782193f37d1d83dab22ff4f0526dc793.

--- fullnodes/cosmos-rosetta/default.nix
+++ fullnodes/cosmos-rosetta/default.nix
@@ -7,7 +7,6 @@ rec {
     vendorSha256 = {
       "0.1.0" = "12qrqlk2x674as07whhh23ga4qj43fz0636fvh6i9xbndabfpp1z";
       "0.1.1" = "0z4njrqya5lr7h07idqx195wnafs83ix0s5hm6kbax3sg6rxpzdq";
-      "0.2.0" = "0mm9vvcjcvps3iklhszmpf7f903x816ma4r4sb3q9risa6wirgpa";
     }.${version} or (builtins.trace "Cosmos Rosetta gateway: using dummy vendor SHA256" "0000000000000000000000000000000000000000000000000000");
 
     src = builtins.fetchGit {

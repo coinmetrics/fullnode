b639b55e8789919571b4f84b246f193f57e32f64
Alexander Bich <quyse0@gmail.com>
2020-12-13 11:15:50 +0300

    monero 0.17.1.7

--- fullnodes/monero/default.nix
+++ fullnodes/monero/default.nix
@@ -21,6 +21,7 @@ rec {
         "0.17.1.3" = "1ddkdfd8i5q509qziwcx1f6nm8axs4a1ppzv2y5lgsqpq375if6j";
         "0.17.1.5" = "0yy9n2qng02j314h8fh5n0mcy6vpdks0yk4d8ifn8hj03f3g2c8b";
         "0.17.1.6" = "0b6zyr3mzqvcxf48i2g45gr649x6nhppik5598jsvg0z7i2hxb9q";
+        "0.17.1.7" = "1fdw4i4rw87yz3hz4yc1gdw0gr2mmf9038xaw2l4rrk5y50phjp4";
       }.${version} or (builtins.trace "Monero fullnode: using dummy SHA256" "0000000000000000000000000000000000000000000000000000");
     };
 

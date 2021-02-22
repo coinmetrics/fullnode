a50769cd092eabc44118c490a7d69c90bbf3013f
Brian Cole <brian@coinmetrics.io>
2021-02-22 22:20:17 +0000

    decred 1.6.1

--- fullnodes/decred/default.nix
+++ fullnodes/decred/default.nix
@@ -7,6 +7,7 @@ rec {
     vendorSha256 = {
       "1.5.2" = "0f02ip0rprhfd04ifbin8zvacpyzj8gzy5vfvlh2a965a4fn78fk";
       "1.6.0" = "0jjz4n1bdd0r86pf8dxqdah1hsqwmckasqlh9i6zzm9qz9ncc4i7";
+      "1.6.1" = "0jjz4n1bdd0r86pf8dxqdah1hsqwmckasqlh9i6zzm9qz9ncc4i7";
     }.${version} or (builtins.trace "Decred fullnode: using dummy vendor SHA256" "0000000000000000000000000000000000000000000000000000");
 
     src = builtins.fetchGit {

343e3445aef628fdd5791f3dc887e9048ca802f6
Alex Wied <alexwied@coinmetrics.io>
2021-02-24 01:47:26 -0500

    [polkadot] 0.8.28-1

--- fullnodes/polkadot/default.nix
+++ fullnodes/polkadot/default.nix
@@ -4,8 +4,8 @@ rec {
     pname = "polkadot";
     inherit version;
     cargoSha256 = {
-      "0.8.26" = "0mjkiq6kih5vbmbmr3nmzw4zynha93lsqwbvams73871124mj7rc";
       "0.8.26-1" = "1581xvarmcq5rgwpincc031q9wxaq4by4xwdvm45sh6n3xjf714v";
+      "0.8.28-1" = "12l8d2bp4vrhvwhmc0l317krwphxmzdzfpq9hpvairpif3hkhgv7";
     }.${version} or (builtins.trace "Polkadot fullnode: using dummy cargo SHA256" "0000000000000000000000000000000000000000000000000000");
     src = builtins.fetchGit {
       url = "https://github.com/paritytech/polkadot.git";
@@ -20,7 +20,7 @@ rec {
     # building WASM binary is complicated
     # see https://github.com/NixOS/nixpkgs/pull/98785
     # and https://gitlab.w3f.tech/florian/w3fpkgs/-/blob/master/pkgs/parity-polkadot/default.nix
-    BUILD_DUMMY_WASM_BINARY = 1;
+    SKIP_WASM_BUILD = 1;
 
     runVend = true;
     doCheck = false;

05fc91295255fc7207be21ec62a61238cca9b97c
Brian Cole <brian@coinmetrics.io>
2021-03-04 21:30:03 +0000

    cosmos 4.0.5

--- fullnodes/cosmos/default.nix
+++ fullnodes/cosmos/default.nix
@@ -9,6 +9,7 @@ with nixpkgs; rec {
       "2.0.14" = "1wbv8v26z603j477g5754j8r4qqzl6m5hmydabz6ws3lgjadvqps";
       "2.0.15" = "1wbv8v26z603j477g5754j8r4qqzl6m5hmydabz6ws3lgjadvqps";
       "4.0.3" = "1070mnjq0gs64mbwpwjy3c8v6nv9g7i1bllgp01f5k49skqw2q9j";
+      "4.0.5" = "0kjq7h71liywzjwnhlcbvzl5gs2g7ihwpvpa4awk0f0z0b9wi3ix";
     }.${version} or (builtins.trace "Cosmos fullnode: using dummy vendor SHA256" "0000000000000000000000000000000000000000000000000000");
 
     src = builtins.fetchGit {

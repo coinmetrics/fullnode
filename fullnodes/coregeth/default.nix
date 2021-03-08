73e260356c0d7a14416397c6d6da027c3ba4218c
Brian Cole <brian@coinmetrics.io>
2021-03-08 21:07:46 +0000

    coregeth 1.11.22 hash

--- fullnodes/coregeth/default.nix
+++ fullnodes/coregeth/default.nix
@@ -9,6 +9,7 @@ rec {
       "1.11.18" = "059zm6w0v7khaqslfjnfwsb908f6xz999yknhr1ihx1wyzirkkp3";
       "1.11.19" = "059zm6w0v7khaqslfjnfwsb908f6xz999yknhr1ihx1wyzirkkp3";
       "1.11.20" = "0332gw2x0lfxc8x8dnmshid0ar1sxfd3g57zp7mdaqbjib207cvb";
+      "1.11.22" = "0ickqy2hlgqmjy0b8ghpw1fk7szxzgmd00hqpj1m02bknmmxss57";
     }.${version} or (builtins.trace "CoreGeth fullnode: using dummy vendor SHA256" "0000000000000000000000000000000000000000000000000000");
 
     src = builtins.fetchGit {

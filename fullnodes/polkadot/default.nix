3f0ac56390453cb29922069f5cc8b4e12007d8f8
Brian Cole <brian@coinmetrics.io>
2021-04-08 19:55:33 +0000

    pokadot 0.8.30

--- fullnodes/polkadot/default.nix
+++ fullnodes/polkadot/default.nix
@@ -7,6 +7,7 @@ rec {
       "0.8.26-1" = "1581xvarmcq5rgwpincc031q9wxaq4by4xwdvm45sh6n3xjf714v";
       "0.8.28-1" = "12l8d2bp4vrhvwhmc0l317krwphxmzdzfpq9hpvairpif3hkhgv7";
       "0.8.29" = "1mmi1aaxsv55flvl5dggnvxxxsn18nvh9vmic9kqgxkvnwi92ng1";
+      "0.8.30" = "1xcadx5vnnaygqksyh58qc2jjgpg7hfgi3149l64aaf72316vi84";
     }.${version} or (builtins.trace "Polkadot fullnode: using dummy cargo SHA256" "0000000000000000000000000000000000000000000000000000");
     src = builtins.fetchGit {
       url = "https://github.com/paritytech/polkadot.git";

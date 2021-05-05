668d7ee6a8186736b1b53b734577e86c3d2a4b97
Brian Cole <brian@coinmetrics.io>
2021-05-05 17:58:40 +0000

    geth 1.10.3 hash

--- fullnodes/geth/default.nix
+++ fullnodes/geth/default.nix
@@ -7,6 +7,7 @@ rec {
     vendorSha256 = {
       "1.10.1" = "186zyqmvj39d3s2bgrah0nw4pcqwswvf7wrzx2krbm34k6z8w30f";
       "1.10.2" = "1idqrlj1as05s77hzzrjb7z1x6kg63wdn2iasc76b4jdbynp1fm8";
+      "1.10.3" = "1q3mg0x9c3xh731pd5gf739nkq7jmbxhal8zxqh3lxslq51maf7k";
     }.${version} or (builtins.trace "Geth fullnode: using dummy vendor SHA256" "0000000000000000000000000000000000000000000000000000");
 
     src = builtins.fetchGit {

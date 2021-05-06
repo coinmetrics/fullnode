23bc49545191bcfa77ad673265d9aaed8808bb36
Brian Cole <brian@coinmetrics.io>
2021-05-06 04:37:41 +0000

    openethereum 3.2.5

--- fullnodes/openethereum/default.nix
+++ fullnodes/openethereum/default.nix
@@ -1,35 +1,7 @@
 { nixpkgs, version }:
 rec {
-  package = with nixpkgs; rustPlatform.buildRustPackage {
-    pname = "openethereum";
-    inherit version;
-
-    cargoSha256 = {
-      "3.2.3" = "1qgl15sd32s0r4q5xmx7lyp92sc81hd4ddc18ynbxjdxgl7knm4k";
-      "3.2.4" = "1gm02pcfll362add8a0dcb0sk0mag8z0q23b87yb6fs870bqvhib";
-    }.${version} or lib.fakeSha256;
-
-    src = builtins.fetchGit {
-      url = "https://github.com/openethereum/openethereum.git";
-      ref = "refs/tags/v${version}";
-    };
-
-    LIBCLANG_PATH = "${llvmPackages.libclang}/lib";
-
-    nativeBuildInputs = [
-      cmake
-      llvmPackages.clang
-      llvmPackages.libclang
-      pkg-config
-    ];
-
-    buildInputs = [ openssl ]
-    ++ lib.optionals stdenv.isLinux [ systemd ]
-    ++ lib.optionals stdenv.isDarwin [ darwin.Security ];
-
-    cargoBuildFlags = [ "--features final" ];
-
-    doCheck = false;
+  package = nixpkgs.callPackage (./. + "/openethereum-${version}.nix") {
+    stdenv = nixpkgs.llvmPackages_12.stdenv;
   };
 
   imageConfig = {

a71cc0ecb2993076be89066a178d46854f9042c2
Alex Wied <alexwied@coinmetrics.io>
2021-05-20 14:13:04 -0400

    [polkadot] 0.9.2

--- fullnodes/polkadot/default.nix
+++ fullnodes/polkadot/default.nix
@@ -3,20 +3,20 @@ rec {
   package = with nixpkgs; rustPlatform.buildRustPackage {
     pname = "polkadot";
     inherit version;
+
     cargoSha256 = {
-      "0.8.26-1" = "1581xvarmcq5rgwpincc031q9wxaq4by4xwdvm45sh6n3xjf714v";
-      "0.8.28-1" = "12l8d2bp4vrhvwhmc0l317krwphxmzdzfpq9hpvairpif3hkhgv7";
-      "0.8.29" = "1mmi1aaxsv55flvl5dggnvxxxsn18nvh9vmic9kqgxkvnwi92ng1";
       "0.8.30" = "1xcadx5vnnaygqksyh58qc2jjgpg7hfgi3149l64aaf72316vi84";
-    }.${version} or (builtins.trace "Polkadot fullnode: using dummy cargo SHA256" "0000000000000000000000000000000000000000000000000000");
+      "0.9.2"  = "0gg42b6h8782wny3dr9gc38wl6bybyf4smashchgrpc649ds6w0a";
+    }.${version} or lib.fakeSha256;
+
     src = builtins.fetchGit {
       url = "https://github.com/paritytech/polkadot.git";
       ref = "refs/tags/v${version}";
     };
 
-    nativeBuildInputs = [ clang ];
+    nativeBuildInputs = [ llvmPackages_12.clang ];
     buildInputs = [ pkgconfig openssl ];
-    LIBCLANG_PATH = "${llvmPackages.libclang}/lib";
+    LIBCLANG_PATH = "${llvmPackages_12.libclang.lib}/lib";
     PROTOC = "${protobuf}/bin/protoc";
 
     # building WASM binary is complicated

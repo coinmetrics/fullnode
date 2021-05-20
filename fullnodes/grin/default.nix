d589f2d1b03e66301de004ba2947468106e0b953
Alex Wied <alexwied@coinmetrics.io>
2021-05-20 13:45:44 -0400

    [grin] 5.1.0

--- fullnodes/grin/default.nix
+++ fullnodes/grin/default.nix
@@ -3,27 +3,22 @@ rec {
   package = with nixpkgs; rustPlatform.buildRustPackage {
     pname = "grin";
     inherit version;
+
     cargoSha256 = {
-      "3.1.0" = "1kvgiqr8114aq4qxmlkdpvlcn48mdws5j33wk0ik184y0i8zb4gm";
-      "3.1.1" = "1z98yd6hr4vz0fcxfrvm7678vdkc7gzafa1ci9lm0vs43m9l6nbp";
-      "4.0.0" = "1pw9kc18whb421y3gw6x9q25brh52izbdz3p95jm2jkdkm9j29xn";
-      "4.0.1" = "1n9k9jd089m4bmnw8gz5bvz923q2ddddqdn9mhi97lxwnvmq8idi";
-      "4.0.2" = "0k2gq8dl5d7sfqybs6r0q27hgm55lh2cfzknkhl0hyg8zl1i6ci4";
-      "4.1.0" = "0x79q943v0xw1hlgk1sgx1xkjj6gsyv15jr9q1xagchlk6hwvblg";
-      "4.1.1" = "0cny8rbi9mg367x3snh9ckx2r45ykp9x5rl5ipynm74cg2r34chs";
-      "5.0.1" = "07i2vsm6d98nwv2mz8nqqns8i594kvz8src0np6qbhs8281w0fq5";
       "5.0.4" = "0afa34yyc7z3a54rhz43396vd0r62wjgql3z6rvmpnr927ch8qli";
-    }.${version} or (builtins.trace "Grin fullnode: using dummy cargo SHA256" "0000000000000000000000000000000000000000000000000000");
+      "5.1.0" = "12ciinv2136x8vr011v71ixlhixnrwifjfx36kkj1nblrrg0vawd";
+    }.${version} or lib.fakeSha256;
+
     src = builtins.fetchGit {
       url = "https://github.com/mimblewimble/grin.git";
       ref = "refs/tags/v${version}";
     };
 
-    nativeBuildInputs = [ llvmPackages.clang ];
+    nativeBuildInputs = [ llvmPackages_12.clang ];
 
-    buildInputs = [ ncurses llvmPackages.libclang ];
+    buildInputs = [ ncurses ];
 
-    LIBCLANG_PATH = "${llvmPackages.libclang}/lib";
+    LIBCLANG_PATH = "${llvmPackages_12.libclang.lib}/lib";
   };
 
   imageConfig = {

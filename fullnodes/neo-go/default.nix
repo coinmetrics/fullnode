ab05e662da12000ba1c53e4f43318d252da344cf
Alex Wied <alexwied@coinmetrics.io>
2021-06-07 12:58:05 -0400

    [neo-go] Remove 0.95.1
    See: https://coinmetrics.atlassian.net/wiki/spaces/DEV/pages/607944710/Fullnodes+Maintenance#Neo-Go

--- fullnodes/neo-go/default.nix
+++ fullnodes/neo-go/default.nix
@@ -7,7 +7,6 @@ rec {
     vendorSha256 = {
       "0.78.2" = "11fvf1lnxbwxvks2vnnl4kkhzgx1aarhfhv9xvbhp206zdz255l5";
       "0.78.3" = "11fvf1lnxbwxvks2vnnl4kkhzgx1aarhfhv9xvbhp206zdz255l5";
-      "0.95.1" = "01lszw1m3q7lx5aq1whzy0gbsqc53vzh282ww4ddxfxgv748j1y3";
     }.${version} or (builtins.trace "NEO Go fullnode: using dummy vendor SHA256" "0000000000000000000000000000000000000000000000000000");
 
     src = builtins.fetchGit {

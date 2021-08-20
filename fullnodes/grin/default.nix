8240f39a1ab4d01157d614b47a62f3e1d6bca9fb
Brian Cole <brian@coinmetrics.io>
2021-08-20 20:45:45 +0000

    grin5.1.1 cargoSha256

--- fullnodes/grin/default.nix
+++ fullnodes/grin/default.nix
@@ -7,6 +7,7 @@ rec {
     cargoSha256 = {
       "5.0.4" = "1fxq7ybb5immakjdgsy60y730jjfwj8nd148sw7nphn2m03brjz5";
       "5.1.0" = "12ciinv2136x8vr011v71ixlhixnrwifjfx36kkj1nblrrg0vawd";
+      "5.1.1" = "11l46ajihgndqdf906iqmwgmdc535rkby8l1n90w74a0j36vpamz";
     }.${version} or lib.fakeSha256;
 
     src = builtins.fetchGit {

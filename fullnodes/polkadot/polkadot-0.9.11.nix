017651e1d390ed90fa7b17953bf3dc3b4ee99888
Brian Cole <brian@coinmetrics.io>
2021-10-12 11:56:45 -0400

    add polkadot-0.9.11.nix

---
+++ fullnodes/polkadot/polkadot-0.9.11.nix
@@ -0,0 +1,42 @@
+{ clang
+, fetchFromGitHub
+, lib
+, llvmPackages
+, protobuf
+, rustPlatform
+}:
+rustPlatform.buildRustPackage rec {
+  pname = "polkadot";
+  version = "0.9.11";
+
+  src = fetchFromGitHub {
+    owner = "paritytech";
+    repo = "polkadot";
+    rev = "v${version}";
+    sha256 = "1nz2b0g9aq3vklsj8xy7ym5mlami5964xaqr4w9r8i49wi61laq9";
+  };
+
+  cargoSha256 = "0000000000000000000000000000000000000000000000000000";
+
+  nativeBuildInputs = [ clang ];
+
+  LIBCLANG_PATH = "${llvmPackages.libclang.lib}/lib";
+  PROTOC = "${protobuf}/bin/protoc";
+
+  # NOTE: We don't build the WASM runtimes since this would require a more
+  # complicated rust environment setup and this is only needed for developer
+  # environments. The resulting binary is useful for end-users of live networks
+  # since those just use the WASM blob from the network chainspec.
+  SKIP_WASM_BUILD = 1;
+
+  # We can't run the test suite since we didn't compile the WASM runtimes.
+  doCheck = false;
+
+  meta = with lib; {
+    description = "Polkadot Node Implementation";
+    homepage = "https://polkadot.network";
+    license = licenses.gpl3Only;
+    maintainers = with maintainers; [ akru andresilva asymmetric FlorianFranzen RaghavSood ];
+    platforms = platforms.linux;
+  };
+}

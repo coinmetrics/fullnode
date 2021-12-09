93dc9080f75063e0c139ee36a6f4732458fd84a7
Alex Wied <alexwied@coinmetrics.io>
2021-12-09 16:22:05 -0500

    [openethereum] 3.3.2

---
+++ fullnodes/openethereum/openethereum-3.3.2.nix
@@ -0,0 +1,55 @@
+{ lib
+, fetchFromGitHub
+, rustPlatform
+, cmake
+, llvmPackages
+, openssl
+, pkg-config
+, stdenv
+, systemd
+, darwin
+}:
+
+rustPlatform.buildRustPackage.override { stdenv = stdenv; } rec {
+  pname = "openethereum";
+  version = "3.3.2";
+
+  src = fetchFromGitHub {
+    owner = "openethereum";
+    repo = "openethereum";
+    rev = "v${version}";
+    sha256 = "sha256-GPlUbfFqsmi4J3f3bE/FW9Go/P6C8vfFmQKWeWkGaD0=";
+  };
+
+  cargoPatches = [
+    ./patches/000-Cargo.lock-3.3.2.patch
+    ./patches/001-logos-3.3.2.patch
+  ];
+
+  cargoSha256 = "sha256-krQtjL/r6p/af6b87arJha0LmhChJm91Vl3pNPWhC24=";
+
+  nativeBuildInputs = [ cmake pkg-config ];
+
+  buildInputs = [ openssl ]
+    ++ lib.optionals stdenv.isLinux [ systemd ]
+    ++ lib.optionals stdenv.isDarwin [ darwin.Security ];
+
+  cargoBuildFlags = [ "--features final" ];
+
+  # Fix tests by preventing them from writing to /homeless-shelter.
+  preCheck = ''
+    export HOME=$(mktemp -d)
+  '';
+
+  # Exclude some tests that don't work in the sandbox
+  # - Nat test requires network access
+  checkFlags = "--skip configuration::tests::should_resolve_external_nat_hosts";
+
+  meta = with lib; {
+    description = "Fast, light, robust Ethereum implementation";
+    homepage = "http://parity.io/ethereum";
+    license = licenses.gpl3;
+    maintainers = with maintainers; [ akru xrelkd ];
+    platforms = lib.platforms.unix;
+  };
+}

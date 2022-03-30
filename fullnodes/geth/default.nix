223e5e0de71b0e22fe192bde666f17525899376f
Brian Cole <brian@coinmetrics.io>
2022-03-30 18:28:07 +0000

    geth 1.10.17

--- fullnodes/geth/default.nix
+++ fullnodes/geth/default.nix
@@ -8,6 +8,7 @@ rec {
       "1.10.14" = "sha256-2H2+i+LweJ3Ip1NUFX/d54wD4ukstZDdhXQydFXUlZM=";
       "1.10.15" = "sha256-2H2+i+LweJ3Ip1NUFX/d54wD4ukstZDdhXQydFXUlZM=";
       "1.10.16" = "sha256-gb7CB/Bzn/kJKV36WatrJc9yjsNcQByCtt3xnfyEreE=";
+      "1.10.17" = "sha256-KlZTqKjAkkLYcEUAKEmNKpS19P29DzIXTuDK8vhB+20=";
     }.${version} or (builtins.trace "Geth fullnode: using dummy vendor SHA256" "0000000000000000000000000000000000000000000000000000");
 
     src = builtins.fetchGit {

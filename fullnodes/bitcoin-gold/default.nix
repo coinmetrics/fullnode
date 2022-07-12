173f53ee6fe5357a7b5b460283314751d9db6e8e
Alex Wied <alexwied@coinmetrics.io>
2022-07-12 19:15:50 -0400

    [bitcoin-gold] Refactor

--- fullnodes/bitcoin-gold/default.nix
+++ fullnodes/bitcoin-gold/default.nix
@@ -1,38 +1,8 @@
 { nixpkgs, version }:
 rec {
-  package = with nixpkgs; stdenv.mkDerivation rec {
-    pname = "bitcoin-gold";
-    inherit version;
-
-    src = builtins.fetchGit {
-      url = "https://github.com/BTCGPU/BTCGPU.git";
-      ref = "refs/tags/v${version}";
-    };
-
-    nativeBuildInputs = [ pkgconfig autoreconfHook ];
-
-    buildInputs = [ boost libevent openssl libsodium libb2 ];
-
-    configureFlags = [
-      "--with-boost-libdir=${boost.out}/lib"
-      "--disable-shared"
-      "--disable-wallet"
-      "--disable-bench"
-      "--disable-tests"
-    ];
-
-    patches = [
-      ./build-fix.patch
-    ] ++
-      # https://github.com/BTCGPU/BTCGPU/commit/ac59db66c0c7631fedd1150c2b61bd1c2878f8c9
-      nixpkgs.lib.optional (builtins.compareVersions version "0.17.3" < 0) (nixpkgs.fetchpatch {
-        url = "https://github.com/BTCGPU/BTCGPU/commit/ac59db66c0c7631fedd1150c2b61bd1c2878f8c9.diff";
-        sha256 = "1l2dqcdnfkpjs5k7yxg72601j6916zaxbb9b194qrz1nqjrw5mj2";
-      });
-
-    doCheck = false;
-
-    enableParallelBuilding = true;
+  package = nixpkgs.callPackage (./. + "/bitcoin-gold-${version}.nix") {
+    withGui = false;
+    withWallet = false;
   };
 
   imageConfig = {
@@ -40,5 +10,10 @@ rec {
       Entrypoint = [ "${package}/bin/bgoldd" ];
       User = "1000:1000";
     };
+
+    extraCommands = ''
+      mkdir ./bin && \
+      ln -s ${package}/bin/bgoldd ./bin/bgoldd
+    '';
   };
 }

0ac0b17d422a58b1cb5e4bc8c0c8f17fd2b16804
Alex Wied <alexwied@coinmetrics.io>
2022-07-13 06:30:15 -0400

    [coregeth] Remove old versions

--- fullnodes/coregeth/default.nix
+++ fullnodes/coregeth/default.nix
@@ -5,8 +5,6 @@ rec {
     inherit version;
 
     vendorSha256 = {
-      "1.12.3"  = "sha256-dq+wMw3sZ3G5ESq8esU5VX4mZFyZdMLFDkx9Qanz8os=";
-      "1.12.4"  = "sha256-dq+wMw3sZ3G5ESq8esU5VX4mZFyZdMLFDkx9Qanz8os=";
       "1.12.6"  = "sha256-dq+wMw3sZ3G5ESq8esU5VX4mZFyZdMLFDkx9Qanz8os=";
       "1.12.7"  = "sha256-xvAgMwkO/sVrwk9SCwIXhgttntzmEY6HCi1dM3PJ35Q=";
     }.${version} or (builtins.trace "CoreGeth fullnode: using dummy vendor SHA256" "0000000000000000000000000000000000000000000000000000");

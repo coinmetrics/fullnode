9a82c9b475299d0c05cc88bda719165445c3d6bd
Alex Wied <alexwied@coinmetrics.io>
2022-07-14 10:55:50 -0400

    [geth] Update hash, remove old versions

--- fullnodes/geth/default.nix
+++ fullnodes/geth/default.nix
@@ -5,13 +5,8 @@ rec {
     inherit version;
 
     vendorSha256 = {
-      "1.10.14" = "sha256-2H2+i+LweJ3Ip1NUFX/d54wD4ukstZDdhXQydFXUlZM=";
-      "1.10.15" = "sha256-2H2+i+LweJ3Ip1NUFX/d54wD4ukstZDdhXQydFXUlZM=";
-      "1.10.16" = "sha256-gb7CB/Bzn/kJKV36WatrJc9yjsNcQByCtt3xnfyEreE=";
-      "1.10.17" = "sha256-KlZTqKjAkkLYcEUAKEmNKpS19P29DzIXTuDK8vhB+20=";
-      "1.10.18" = "sha256-ei0CkaH9VQiQCq1qEON6hAqgXilwmYRCTEAro6iQ2kg=";
       "1.10.19" = "sha256-MZCX0Io7dMVas1YDjPli98MdheG3J18g5UYAVCIii3k=";
-      "1.10.20" = "sha256-MN+5CXlujvidXR8bHqq3VBNzZI/nfVPdEUme32+sygk=";
+      "1.10.20" = "sha256-MZCX0Io7dMVas1YDjPli98MdheG3J18g5UYAVCIii3k=";
     }.${version} or (builtins.trace "Geth fullnode: using dummy vendor SHA256" "0000000000000000000000000000000000000000000000000000");
 
     src = builtins.fetchGit {

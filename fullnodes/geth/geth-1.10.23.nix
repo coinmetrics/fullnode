4727d01cc35bf6c488ef42fb49b1a2b81c1eb0c7
Brian Cole <brian@coinmetrics.io>
2022-08-24 16:20:37 +0000

    geth 1.10.23

--- fullnodes/geth/geth-1.10.23.nix
+++ fullnodes/geth/geth-1.10.23.nix
@@ -1,15 +1,16 @@
 { buildGoModule, fetchFromGitHub }:
 buildGoModule rec {
   pname = "geth";
-  version = "1.10.22";
+  version = "1.10.23";
 
   vendorSha256 = "sha256-eR/qu9QAd3+0R3XYP0M17+wXL9dRVDjW0Af9PRchrps=";
+  #vendorSha256 = "sha256-0000000000000000000000000000000000000000000=";
 
   src = fetchFromGitHub {
     owner = "ethereum";
     repo = "go-ethereum";
     rev = "refs/tags/v${version}";
-    sha256 = "sha256-qaM1I3ytMZN+5v/Oj47n3Oc21Jk7DtjfWA/xDprbn/M=";
+    hash = "sha256-1fEmtbHKrjuyIVrGr/vTudZ99onkNjEMvyBJt4I8KK4=";
   };
 
   proxyVendor = true;

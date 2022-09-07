5ee25482203fcb516597faac9260ff8ea24674ee
Alex Wied <alexwied@coinmetrics.io>
2022-09-07 14:43:36 -0400

    Refactor support for private images

--- flake.nix
+++ flake.nix
@@ -102,10 +102,10 @@
 
           for i in "''${!images[@]}"; do
             [[ -n ''${CI_REGISTRY_IMAGE+x} ]] && skopeo --insecure-policy copy -f oci docker-archive:"''${images[i]}" docker://"$CI_REGISTRY_IMAGE"/${fullnode}:"''${tags[i]}"
-            if [[ $PUBLIC_IMAGE == "true" ]]; do
-              skopeo --insecure-policy copy -f oci docker-archive:"''${images[i]}" docker://docker.io/coinmetrics/${fullnode}:"''${tags[i]}"
-            else
+            if [[ $PRIVATE == "true" ]]; then
               echo "Image marked private: Skipping publish to Docker Hub."
+            else
+              skopeo --insecure-policy copy -f oci docker-archive:"''${images[i]}" docker://docker.io/coinmetrics/${fullnode}:"''${tags[i]}"
             fi
           done
         '';

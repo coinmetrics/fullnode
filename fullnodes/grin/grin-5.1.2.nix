aca23f08fb7c1efcc6eb11b998dabb8627b6ddea
Alex Wied <alexwied@coinmetrics.io>
2022-10-19 19:51:41 -0400

    [grin] socket2: 0.3.12 -> 0.3.19
    In Rust 1.64, a compile error is thrown as a result of the socket2
    library.

--- fullnodes/grin/grin-5.1.2.nix
+++ fullnodes/grin/grin-5.1.2.nix
@@ -3,14 +3,20 @@ rustPlatform.buildRustPackage {
   pname = "grin";
   version = "5.1.2";
 
-  cargoSha256 = "sha256-LQWO50XCaq+gYtsX4sZtWineKRLw14PjZMnCTzckifg=";
-
   src = fetchFromGitHub {
     owner = "mimblewimble";
     repo = "grin";
     rev = "83e8ec65b3e5b2268b29e68dc1cdc1aa031e9147";
     sha256 = "sha256-yKOhQV0fSIJoOEgqb0ic371qnhGXGXZo8sCrkIYoDJU=";
   };
+
+  cargoLock = {
+    lockFile = ./patches/5.1.2-Cargo.lock;
+  };
+
+  postPatch = ''
+    cp ${./patches/5.1.2-Cargo.lock} Cargo.lock
+  '';
 
   nativeBuildInputs = [ llvmPackages_12.clang ];
 

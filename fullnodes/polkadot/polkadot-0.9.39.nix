3088cfe55548db9cd1181498ecbe1de4431a48a2
Brian Cole <brian@coinmetrics.io>
2023-03-02 19:41:05 +0000

    polkadot 0.9.39

---
+++ fullnodes/polkadot/polkadot-0.9.39.nix
@@ -0,0 +1,43 @@
+{ clang
+, fetchFromGitHub
+, lib
+, llvmPackages
+, protobuf
+, rustPlatform
+}:
+rustPlatform.buildRustPackage rec {
+  pname = "polkadot";
+  version = "0.9.39";
+
+  src = fetchFromGitHub {
+    owner = "paritytech";
+    repo = "polkadot";
+    rev = "v${version}";
+    hash = "sha256-++aSGovKRE4+1hRoDqo6lSO4aenNrdvkVqaIXz4s0bk=";
+  };
+
+  #cargoHash = "sha256-0000000000000000000000000000000000000000000=";
+  cargoHash = "sha256-KqdUh+ES9UB01yrmNVXdiyYZPh9pjJOEjcuwlJr6Jxc=";
+
+  nativeBuildInputs = [ clang ];
+
+  LIBCLANG_PATH = "${llvmPackages.libclang.lib}/lib";
+  PROTOC = "${protobuf}/bin/protoc";
+
+  # NOTE: We don't build the WASM runtimes since this would require a more
+  # complicated rust environment setup and this is only needed for developer
+  # environments. The resulting binary is useful for end-users of live networks
+  # since those just use the WASM blob from the network chainspec.
+  SKIP_WASM_BUILD = 1;
+
+  # We can't run the test suite since we didn't compile the WASM runtimes.
+  doCheck = false;
+
+  meta = with lib; {
+    description = "Polkadot Node Implementation";
+    homepage = "https://polkadot.network";
+    license = licenses.gpl3Only;
+    maintainers = with maintainers; [ akru andresilva asymmetric FlorianFranzen RaghavSood ];
+    platforms = platforms.linux;
+  };
+}

756fe2475247be296d45b228992f0c05af17ebf6
Brian Cole <brian@coinmetrics.io>
2023-03-02 21:25:21 +0000

    litecoin 0.21.2.2

---
+++ fullnodes/litecoin/litecoin-0.21.2.2.nix
@@ -0,0 +1,29 @@
+{ autoreconfHook, boost17x, db48, fetchFromGitHub, fmt, libevent, openssl
+, pkg-config, stdenv, zeromq }:
+stdenv.mkDerivation rec {
+  pname = "litecoin";
+  version = "0.21.2.2";
+
+  src = fetchFromGitHub {
+    owner = "litecoin-project";
+    repo = "litecoin";
+    rev = "v${version}";
+    hash = "sha256-TuDc47TZOEQA5Lr4DQkEhnO/Szp9h71xPjaBL3jFWuM=";
+  };
+
+  nativeBuildInputs = [ autoreconfHook pkg-config ];
+
+  buildInputs = [ boost17x db48 fmt libevent openssl zeromq ];
+
+  configureFlags = [
+    "--with-boost-libdir=${boost17x.out}/lib"
+    "--disable-shared"
+    "--disable-wallet"
+    "--disable-bench"
+    "--disable-tests"
+  ];
+
+  doCheck = false;
+
+  enableParallelBuilding = true;
+}

ba4ec4ca491faf557674e8bd0365a9f9861c59eb
Alex Wied <alexwied@coinmetrics.io>
2023-05-04 02:43:05 -0400

    [erigon] 2.43.0

--- fullnodes/erigon/erigon-2.43.0.nix
+++ fullnodes/erigon/erigon-2.43.0.nix
@@ -1,16 +1,16 @@
 { buildGoModule, fetchFromGitHub }:
 buildGoModule rec {
   pname = "erigon";
-  version = "2.42.0";
-
-  vendorSha256 = "sha256-gY47bf/bgDU7biGKlZ5g8ZpNYVC6ryH5krHCNzmu+4Q=";
+  version = "2.43.0";
 
   src = fetchFromGitHub {
     owner = "ledgerwatch";
     repo = "erigon";
     rev = "refs/tags/v${version}";
-    hash = "sha256-BWKnFy1BO6oiTZuD3+9qHT9nFuVS6OHR3/7omoJIMSg=";
+    hash = "sha256-vPGFaQ7vROuZi57GBZQH+wRE8qoIMRHfSyEGqPvf/rY=";
   };
+
+  vendorSha256 = "sha256-unGca8GefwGRTe3+/phM4ZlaL40rRp9CKFCA5jzj0Zk=";
 
   libsecp256k1-src = fetchFromGitHub {
     owner = "ledgerwatch";
@@ -30,17 +30,15 @@ buildGoModule rec {
     "cmd/erigon"
   ];
 
-  postBuild = ''
-    if [[ "$name" == "${pname}-${version}-go-modules" ]]; then
-      pushd vendor/github.com/ledgerwatch
-      rm -rf secp256k1
-      cp -a ${libsecp256k1-src} secp256k1
-      popd
+  modPostBuild = ''
+    pushd vendor/github.com/ledgerwatch
+    rm -rf secp256k1
+    cp -a ${libsecp256k1-src} secp256k1
+    popd
 
-      pushd vendor/github.com/supranational
-      rm -rf blst
-      cp -a ${blst-src} blst
-      popd
-    fi
+    pushd vendor/github.com/supranational
+    rm -rf blst
+    cp -a ${blst-src} blst
+    popd
   '';
 }

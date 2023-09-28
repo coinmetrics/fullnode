a4992cf7241e3703a363da5bae0b1b512a811dec
Brian Cole <brian@coinmetrics.io>
2023-09-28 17:36:27 +0000

    decred 1.8.1

---
+++ fullnodes/decred/dcrd-1.8.1.nix
@@ -0,0 +1,27 @@
+{ lib, buildGoModule, fetchFromGitHub }:
+
+buildGoModule rec {
+  pname = "dcrd";
+  version = "1.8.1";
+
+  src = fetchFromGitHub {
+    owner = "decred";
+    repo = "dcrd";
+    rev = "refs/tags/release-v${version}";
+    hash = "sha256-nSocqwXgJhvfbdElddbb1gGxoygmtVtK6DbiSuMxYew=";
+  };
+
+  #vendorHash = lib.fakeHash;  # only for getting the right value
+  vendorHash = "sha256-Napcfj1+KjQ21Jb/qpIzg2W/grzun2Pz5FV5yIBXoTo=";
+
+  doCheck = false;
+
+  subPackages = [ "." ];
+
+  meta = {
+    homepage = "https://decred.org";
+    description = "Decred daemon in Go (golang)";
+    license = with lib.licenses; [ isc ];
+    maintainers = with lib.maintainers; [ juaningan ];
+  };
+}

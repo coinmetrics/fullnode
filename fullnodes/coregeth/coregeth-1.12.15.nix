d065330742a0dcb47b433ed06d28235ff42c95c6
Brian Cole <brian@coinmetrics.io>
2023-10-26 17:31:59 +0000

    coregeth 1.12.15

--- fullnodes/coregeth/coregeth-1.12.15.nix
+++ fullnodes/coregeth/coregeth-1.12.15.nix
@@ -1,7 +1,7 @@
 { buildGoModule, fetchFromGitHub }:
 buildGoModule rec {
   pname = "coregeth";
-  version = "1.12.14";
+  version = "1.12.15";
 
   #vendorHash = "sha256-0000000000000000000000000000000000000000000=";
   vendorHash = "sha256-MtE+a3XykBZg6IybTwMCwMsBQ1Be7OQQe2Ukp0pjawo=";
@@ -10,7 +10,7 @@ buildGoModule rec {
     owner = "etclabscore";
     repo = "core-geth";
     rev = "refs/tags/v${version}";
-    hash = "sha256-pkCphXbHs8vUk7WfB0XUJj13r6j75fn46ncLsCuCDWY=";
+    hash = "sha256-u1u4L4GcqIBFyoStBx3JXkrYcingTfKlpQfAwKnz45g=";
   };
 
   proxyVendor = true;

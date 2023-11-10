4ebeb3740d1af3e6db0ed00938c83a24d8c8df8c
Brian Cole <brian@coinmetrics.io>
2023-11-10 19:09:03 +0000

    monero 0.18.3.1

---
+++ fullnodes/monero/monero-0.18.3.1.nix
@@ -0,0 +1,31 @@
+{ boost17x, cmake, fetchFromGitHub, libsodium, ninja, openssl, pkg-config, stdenv
+, unbound, zeromq }:
+stdenv.mkDerivation rec {
+  pname = "monero";
+  version = "0.18.3.1";
+
+  src = fetchFromGitHub {
+    owner = "monero-project";
+    repo = "monero";
+    rev = "refs/tags/v${version}";
+    fetchSubmodules = true;
+    hash = "sha256-ndeCYOgy8dzsP8YAIm+tEj4ndrpVTsRLmk7juvXL1cw=";
+  };
+
+  nativeBuildInputs = [ cmake ninja pkg-config ];
+
+  cmakeFlags = [
+    "-DBUILD_TESTS=OFF"
+    "-DARCH=x86-64"
+    "-DBUILD_64=ON"
+    "-DBUILD_TAG=linux-x64"
+  ];
+
+  configurePhase = "cmakeConfigurePhase";
+
+  buildInputs = [ boost17x libsodium openssl unbound zeromq ];
+
+  doCheck = false;
+
+  enableParallelBuilding = true;
+}

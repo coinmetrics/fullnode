9fed2645a973b39074c668d0267db761d615ae8e
Alex Wied <alexwied@coinmetrics.io>
2023-11-22 13:43:52 -0500

    [grin] Fix 5.1.2

--- fullnodes/grin/grin-5.1.2.nix
+++ fullnodes/grin/grin-5.1.2.nix
@@ -1,4 +1,9 @@
-{ fetchFromGitHub, llvmPackages_12, ncurses, rustPlatform }:
+{ fetchFromGitHub
+, fetchpatch
+, llvmPackages_12
+, ncurses
+, rustPlatform
+}:
 rustPlatform.buildRustPackage {
   pname = "grin";
   version = "5.1.2";
@@ -13,6 +18,14 @@ rustPlatform.buildRustPackage {
   cargoLock = {
     lockFile = ./patches/5.1.2-Cargo.lock;
   };
+
+  patches = [
+    (fetchpatch {
+      name = "fix-leading-zeroes-bugs";
+      url = "https://patch-diff.githubusercontent.com/raw/mimblewimble/grin/pull/3763.patch";
+      hash = "sha256-xFUv0BhOSUbRZ7Q9As7aLcnOQCBiMr4HEyN//O2w9dU=";
+    })
+  ];
 
   postPatch = ''
     cp ${./patches/5.1.2-Cargo.lock} Cargo.lock

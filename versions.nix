ac96ccae8e01de39e50860cbe2ae15089e9cade2
Antoine Le Calvez <antoine+gitlab@coinmetrics.io>
2024-01-04 20:30:06 +0000

    [algorand] 3.21.0

--- versions.nix
+++ versions.nix
@@ -1,5 +1,5 @@
 {
-  algorand = [ "3.19.0" "3.20.1" ];
+  algorand = [ "3.20.1" "3.21.0" ];
   algorand-indexer = [ "2.15.4" "3.3.0" ];
   avalanchego = [ "1.10.16" ];
   avalanche-rosetta = [ "0.1.38" ];

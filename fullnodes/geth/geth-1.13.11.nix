897ae5b700648cd7a62523b1f7d89f9764d84feb
Antoine Le Calvez <antoine@coinmetrics.io>
2024-01-24 17:46:10 +0100

    [geth] 1.13.11

---
+++ fullnodes/geth/geth-1.13.11.nix
@@ -0,0 +1,23 @@
+{ buildGoModule, fetchFromGitHub }:
+buildGoModule rec {
+  pname = "geth";
+  version = "1.13.11";
+
+  src = fetchFromGitHub {
+    owner = "ethereum";
+    repo = "go-ethereum";
+    rev = "v${version}";
+    # hash = "sha256-0000000000000000000000000000000000000000000=";
+    hash = "sha256-GCU9CvIcvXzqT4jaugqgZrueqLGYFDygBObwM667hVo=";
+  };
+
+  # vendorHash = "sha256-0000000000000000000000000000000000000000000=";
+  vendorHash = "sha256-LRB1JQ0O8f9TsoCjc7keXdJj4lkGnfbf6UaLmjevpzM=";
+
+  subPackages = [
+    "cmd/geth"
+  ];
+
+  # https://github.com/ethereum/go-ethereum/blob/ea9e62ca3db5c33aa7438ebf39c189afd53c6bf8/build/ci.go#L212
+  tags = [ "urfave_cli_no_docs" ];
+}

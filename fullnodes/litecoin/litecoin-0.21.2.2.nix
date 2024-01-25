73aa79afd21f1eff8dbc9a632a29fa25bf47b75e
Alex Wied <alexwied@coinmetrics.io>
2024-01-25 12:41:32 -0300

    [litecoin] Add missing includes, remove old version

--- fullnodes/litecoin/litecoin-0.21.2.2.nix
+++ fullnodes/litecoin/litecoin-0.21.2.2.nix
@@ -2,6 +2,7 @@
 , boost177
 , db48
 , fetchFromGitHub
+, fetchpatch
 , fmt
 , libevent
 , openssl
@@ -18,6 +19,13 @@ stdenv.mkDerivation rec {
     rev = "v${version}";
     hash = "sha256-TuDc47TZOEQA5Lr4DQkEhnO/Szp9h71xPjaBL3jFWuM=";
   };
+
+  patches = [
+    (fetchpatch {
+      url = "https://patch-diff.githubusercontent.com/raw/litecoin-project/litecoin/pull/929.patch";
+      hash = "sha256-1y4Iz2plMw5HMAjl9x50QQpYrYaUd2WKrrAcUnQmlBY=";
+    })
+  ];
 
   nativeBuildInputs = [ autoreconfHook pkg-config ];
 

a5030d8061145a571f2702e48ecad49fb61c9f5a
ciaran.donegan1 <ciaran.donegan@coinmetrics.io>
2024-02-07 10:20:41 +0000

    roll back 1.10.19.nix as image was incorrectly built with 1.10.16 values

--- versions.nix
+++ versions.nix
@@ -1,7 +1,7 @@
 {
   algorand = [ "3.20.1" "3.21.0" ];
   algorand-indexer = [ "2.15.4" "3.3.0" "3.4.0"];
-  avalanchego = [ "1.10.16" "1.10.19" ];
+  avalanchego = [ "1.10.16" ];
   avalanche-rosetta = [ "0.1.38" ];
   bitcoin-cash = [ "27.0.0" ];
   bitcoin-gold = [ "0.17.3" ];

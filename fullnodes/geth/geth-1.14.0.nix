304244d583725a7bf10c385c9c83cbc359ab7e96
Antoine Le Calvez <antoine@coinmetrics.io>
2024-04-25 13:39:40 +0200

    Geth 1.14.0

--- fullnodes/geth/geth-1.14.0.nix
+++ fullnodes/geth/geth-1.14.0.nix
@@ -1,18 +1,16 @@
 { buildGoModule, fetchFromGitHub }:
 buildGoModule rec {
   pname = "geth";
-  version = "1.13.12";
+  version = "1.14.0";
 
   src = fetchFromGitHub {
     owner = "ethereum";
     repo = "go-ethereum";
     rev = "v${version}";
-    # hash = "sha256-0000000000000000000000000000000000000000000=";
-    hash = "sha256-2olJV7Z01kuXlUGyI0v4YNW07/RfYiDUhBncCIS4s0A=";
+    hash = "sha256-woEUUC+n5YN+TFFpGWchub9AQroITQZyGUcnvHEniQo=";
   };
 
-  # vendorHash = "sha256-0000000000000000000000000000000000000000000=";
-  vendorHash = "sha256-gcLVQTBpOE0DHz7/p7PENhwghftJKUDm88/4jaQ1VYw=";
+  vendorHash = "sha256-u9m0umel7H3XXV7wwhhuxM8q3o9un5D+KTHagbcu7E0=";
 
   subPackages = [
     "cmd/geth"

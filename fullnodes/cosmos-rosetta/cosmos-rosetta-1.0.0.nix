49affc34cb04e3a7bf28ca9985149e9f563a3097
Alex Wied <alexwied@coinmetrics.io>
2024-05-02 13:57:03 -0400

    [cosmos-rosetta] Use vendorHash instead of vendorSha256

--- fullnodes/cosmos-rosetta/cosmos-rosetta-1.0.0.nix
+++ fullnodes/cosmos-rosetta/cosmos-rosetta-1.0.0.nix
@@ -3,7 +3,7 @@ buildGoModule {
   pname = "cosmos-rosetta";
   version = "1.0.0";
 
-  vendorSha256 = "sha256-e1O0rJpthFiM6vbidNkV01xs7nkqDdeM6PsS26I6IX4=";
+  vendorHash = "sha256-e1O0rJpthFiM6vbidNkV01xs7nkqDdeM6PsS26I6IX4=";
 
   src = fetchFromGitHub {
     owner = "tendermint";

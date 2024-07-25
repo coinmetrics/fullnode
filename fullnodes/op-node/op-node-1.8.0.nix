de5aa7ed545ec473e6cc70ec4e08c29927b6c6e4
Petar Mitrovic <petar@coinmetrics.io>
2024-07-25 12:44:42 +0000

    Bump up optimism to 1.8.0
    <❗️**TODO**❗️: Explanation of the MR changes and optionally verification steps>

--- fullnodes/op-node/op-node-1.8.0.nix
+++ fullnodes/op-node/op-node-1.8.0.nix
@@ -1,16 +1,16 @@
 { buildGoModule, fetchFromGitHub }:
 buildGoModule rec {
   pname = "op-node";
-  version = "1.7.7";
+  version = "1.8.0";
 
   src = fetchFromGitHub {
     owner = "ethereum-optimism";
     repo = "optimism";
     rev = "op-node/v${version}";
-    hash = "sha256-eHXUlPkpgNrwwPUoGedsDrtmAHrYmR1DVXty4UAzLEE=";
+    hash = "sha256-vHlAQ1DG87vcLE67wFmWTxfiIyw1xjO+GeqQ0IJIpDA=";
   };
 
-  vendorHash = "sha256-MWGjRj5SMFi3O86l3Gc/oavzWd1TtoKr53eEXbCOamQ=";
+  vendorHash = "sha256-UQ/appKaBvHkya9RNIYvSd4E+22DDFjJgJ3g82IShNY=";
 
   subPackages = [
     "op-node/cmd"

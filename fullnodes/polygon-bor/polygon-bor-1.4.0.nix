1bbf5167a5f4c9bf6802e7d01d95638070e9c3d9
Petar Mitrovic <petar@coinmetrics.io>
2024-09-16 11:27:42 +0000

    FULLNODE-246 Added polygon/bor 1.4.0

---
+++ fullnodes/polygon-bor/polygon-bor-1.4.0.nix
@@ -0,0 +1,27 @@
+{ buildGoModule, fetchFromGitHub }:
+buildGoModule rec {
+  pname = "polygon-bor";
+  version = "1.4.0";
+
+  src = fetchFromGitHub {
+    owner = "maticnetwork";
+    repo = "bor";
+    rev = "v${version}";
+    hash = "sha256-lncGIpl1p3VSPcUv40cwM2eb9GIiG/QjSHcG1gon+S8=";
+  };
+
+  vendorHash = "sha256-yp/sGhbqMYFtShH32YMViOZCoBP1O0ck/jqwwg3fcfY=";
+
+  subPackages = [
+    "cmd/cli"
+  ];
+
+  proxyVendor = true;
+
+  # The tests may require docker
+  doCheck = false;
+
+  postInstall = ''
+    mv $out/bin/cli $out/bin/bor
+  '';
+}
--- versions.nix
+++ versions.nix
@@ -31,7 +31,7 @@
   op-geth = [ "1.101408.0" ];
   pivx = [ "5.6.1" ];
   polkadot = [ "1.15.1" ];
-  polygon-bor = [ "1.3.4" ];
+  polygon-bor = [ "1.3.4" "1.4.0" ];
   vertcoin = [ "23.2" ];
   zcash = [ "5.10.0" ];
 }

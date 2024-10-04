2c48e6c281e3fb830507857cea77b88a2e462956
Antoine Le Calvez <antoine+gitlab@coinmetrics.io>
2024-10-04 11:24:00 +0000

    [spring] 1.0.2
    https://coinmetrics.atlassian.net/browse/FULLNODE-301

---
+++ fullnodes/spring/spring-1.0.2.nix
@@ -0,0 +1,48 @@
+{ cmake
+, curl
+, fetchFromGitHub
+, gcc
+, git
+, gmp
+, llvm
+, ninja
+, overrideCC
+, python3
+, stdenv
+}:
+
+(overrideCC stdenv gcc).mkDerivation rec {
+  pname = "spring";
+  version = "1.0.2";
+
+  src = fetchFromGitHub {
+    owner = "AntelopeIO";
+    repo = "spring";
+    rev = "v${version}";
+    fetchSubmodules = true;
+    leaveDotGit = true;
+    hash = "sha256-jYwzUWuwTJCJKuVEQksnMxgN2wXaiaAvrJgp1LJdrxA=";
+  };
+
+  patches = [
+    ./patches/fix-threading-issue.patch
+  ];
+
+  nativeBuildInputs = [
+    cmake
+    curl
+    git
+    ninja
+    llvm
+    python3
+  ];
+
+  buildInputs = [
+    gmp
+  ];
+
+  separateDebugInfo = true;
+
+  # Some individual source files require many GB of memory
+  enableParallelBuilding = false;
+}

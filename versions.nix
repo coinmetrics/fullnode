a91d38c32ee5b37047566fc68d61853e2d4fba23
Antoine Le Calvez <antoine+gitlab@coinmetrics.io>
2024-10-15 14:56:21 +0000

    [bitcoin-zmce] 28.0
    I tried building it locally (OSX) outside of Nix and it fails because of some library incompatiblity and inside Nix it never completes.

--- versions.nix
+++ versions.nix
@@ -6,7 +6,7 @@
   bitcoin-cash = [ "27.0.0" ];
   bitcoin-gold = [ "0.17.3" ];
   bitcoin-sv = [ "1.0.16" ];
-  bitcoin-zmce = [ "26.0" "27.0" ];
+  bitcoin-zmce = [ "26.0" "27.0" "28.0" ];
   bsc = [ "1.4.10" ];
   coregeth = [ "1.12.20" ];
   cosmos-gaia = [ "9.0.2" ];

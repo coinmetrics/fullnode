4ab1dcc96e019573616dcc2bf33c2b6b4adc1954
Sergio Penén <sergio.penen@coinmetrics.io>
2025-02-26 13:45:58 +0000

    FULLNODE-702: decred / dcrd: dcrd v2.0.6
    Updated image version with zeroed hashes
    
    Closes FULLNODE-702

---
+++ fullnodes/decred/dcrd-2.0.6.nix
@@ -0,0 +1,26 @@
+{ lib, buildGoModule, fetchFromGitHub }:
+
+buildGoModule rec {
+  pname = "dcrd";
+  version = "2.0.6";
+
+  src = fetchFromGitHub {
+    owner = "decred";
+    repo = "dcrd";
+    rev = "refs/tags/release-v${version}";
+    hash = "sha256-mSq4SRSnZOoCuRKVwmb8Y6+KbaTtg+DLf4YX5oApx0k=";
+  };
+
+  vendorHash = "sha256-kzb8qh1j2+TlX+et0RSq5qU1LHSEs3Kaf0nHOnGjdd0=";
+
+  doCheck = false;
+
+  subPackages = [ "." ];
+
+  meta = {
+    homepage = "https://decred.org";
+    description = "Decred daemon in Go (golang)";
+    license = with lib.licenses; [ isc ];
+    maintainers = with lib.maintainers; [ juaningan ];
+  };
+}

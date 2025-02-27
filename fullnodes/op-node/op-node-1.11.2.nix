dd04c6314c12e1f4991c04f7447379e79ce441bf
Petar Mitrovic <petar@coinmetrics.io>
2025-02-27 20:43:18 +0000

    FULLNODE-716 Added op-node 1.11.2; removed some older versions
    ```
    nix-prefetch-git --rev v1.11.2 --url https://github.com/ethereum-optimism/optimism
    {
      "url": "https://github.com/ethereum-optimism/optimism",
      "rev": "bc6d11a854b4d4cd60d9de541eb60673912d4f76",
      "date": "2025-02-21T16:58:33Z",
      "path": "/nix/store/hzikgfkr1hb72kg6b2bvsv0zlg5fxq9d-optimism",
      "sha256": "1l7jmvylwyb935s27zn42rszvfq529708vkpi1h40pmww31c99r8",
      "hash": "sha256-KKfEwuC8XkBgiHduBE4SBbv9dRbE/iN0GWl5Tv2u8tA=",
      "fetchLFS": false,
      "fetchSubmodules": false,
      "deepClone": false,
      "leaveDotGit": false
    }
    ```

--- fullnodes/op-node/op-node-1.11.2.nix
+++ fullnodes/op-node/op-node-1.11.2.nix
@@ -1,16 +1,16 @@
 { buildGoModule, fetchFromGitHub }:
 buildGoModule rec {
   pname = "op-node";
-  version = "1.9.5";
+  version = "1.11.2";
 
   src = fetchFromGitHub {
     owner = "ethereum-optimism";
     repo = "optimism";
     rev = "op-node/v${version}";
-    hash = "sha256-9ATxrxYbFDOAxzelmWvT3cjmO1j5pLluVu8iBKiXIEM=";
+    hash = "sha256-KKfEwuC8XkBgiHduBE4SBbv9dRbE/iN0GWl5Tv2u8tA=";
   };
 
-  vendorHash = "sha256-1tZXG4eXB9LUZnHgKhNkphkFw2PLZjpelttpsdXAF/M=";
+  vendorHash = "sha256-8V2m+hWZ1m4Zv+QKXMDJmM7SDo1Lpo5FrnxLpWbGkPw=";
 
   subPackages = [
     "op-node/cmd"

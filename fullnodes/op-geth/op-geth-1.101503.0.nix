b4bc70dc55181732f8dfe6a454cee995bfd07404
Antoine Le Calvez <antoine@coinmetrics.io>
2025-03-12 09:36:11 +0100

    [FULLNODE-751] Op-Geth 1.101503.0

--- fullnodes/op-geth/op-geth-1.101503.0.nix
+++ fullnodes/op-geth/op-geth-1.101503.0.nix
@@ -1,16 +1,16 @@
 { buildGoModule, fetchFromGitHub }:
 buildGoModule rec {
   pname = "op-geth";
-  version = "1.101411.8";
+  version = "1.101503.0";
 
   src = fetchFromGitHub {
     owner = "ethereum-optimism";
     repo = "op-geth";
     rev = "v${version}";
-    hash = "sha256-6RD2Qfm1iIQ7VUJXUgE7/49ttHOyGw4kYYbeFnK+p/A=";
+    hash = "sha256-cOJEp7oQaDNyp1M1zeZ5fywf8xs2ypA8AgSPrMPwzlY=";
   };
 
-  vendorHash = "sha256-EfgB88UUzGsI2uE1DAElV3d/cGGZEdZ67sZbYiw8pQk=";
+  vendorHash = "sha256-L0zL+iHFYxPXbfT76M3jYIPAaokDauDLzj3MI4V/76M=";
 
   proxyVendor = true;
 

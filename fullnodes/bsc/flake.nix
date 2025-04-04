6f2d2de23195eb1e49ca0726ac29538bf0e3c6a0
Alex Wied <alexwied@coinmetrics.io>
2025-04-04 15:42:34 -0400

    [bsc] Refactor

---
+++ fullnodes/bsc/flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "BSC";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    utils.url = "path:../..";
+  };
+
+  outputs = { self, flake-utils, nixpkgs, utils }:
+  flake-utils.lib.eachDefaultSystem (system:
+  let
+    pkgs = import nixpkgs { inherit system; };
+
+    makeImageConfig = package: {
+      config = {
+        Entrypoint = [ "${package}/bin/geth" ];
+        User = "1000:1000";
+      };
+    };
+
+    generatedFlake = utils.lib.${system}.makeFlake {
+      inherit makeImageConfig;
+      name = "bsc";
+      version = "1.5.7";
+    };
+  in {
+    packages = generatedFlake.packages;
+    apps = generatedFlake.apps // utils.lib.${system}.loginApp;
+  });
+}

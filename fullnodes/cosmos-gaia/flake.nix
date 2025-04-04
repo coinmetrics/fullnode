46a7c451c48bd4daecd8da916d0733c69d9b6a9e
Alex Wied <alexwied@coinmetrics.io>
2025-04-04 17:56:33 -0400

    [cosmos-gaia] Refactor

---
+++ fullnodes/cosmos-gaia/flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Cosmos Gaia";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    utils.url = "path:../..";
+  };
+
+  outputs = { self, flake-utils, nixpkgs, utils }:
+  flake-utils.lib.eachDefaultSystem (system:
+  let
+    pkgs = import nixpkgs { inherit system; };
+
+    makeImageConfig = package: {
+      config = {
+        Entrypoint = [ "${package}/bin/gaiad" ];
+        User = "1000:1000";
+      };
+    };
+
+    generatedFlake = utils.lib.${system}.makeFlake {
+      inherit makeImageConfig;
+      name = "cosmos-gaia";
+      version = "9.0.2";
+    };
+  in {
+    packages = generatedFlake.packages;
+    apps = generatedFlake.apps // utils.lib.${system}.loginApp;
+  });
+}

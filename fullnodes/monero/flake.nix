1e8c57961cab42ccb086905bb45938cbd0212579
Alex Wied <alexwied@coinmetrics.io>
2025-04-07 18:14:18 -0400

    [monero] Refactor

---
+++ fullnodes/monero/flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Monero";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/7b1d7da3fefe77e8d1d860f7e78bec9cc64b57ae";
+    utils = {
+      url = "path:../..";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, flake-utils, nixpkgs, utils }:
+  flake-utils.lib.eachDefaultSystem (system:
+  let
+    pkgs = import nixpkgs { inherit system; };
+
+    makeImageConfig = package: {
+      config = {
+        Entrypoint = [ "${package}/bin/monerod" ];
+        User = "1000:1000";
+      };
+    };
+
+    generatedFlake = utils.lib.${system}.makeFlake {
+      inherit makeImageConfig;
+      name = "monero";
+      version = "0.18.3.1";
+    };
+  in {
+    packages = generatedFlake.packages;
+    apps = generatedFlake.apps // utils.lib.${system}.loginApp;
+  });
+}

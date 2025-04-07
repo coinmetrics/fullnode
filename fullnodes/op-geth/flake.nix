8366985dc53bef97e4e47fe51e8f923fb8bf1372
Alex Wied <alexwied@coinmetrics.io>
2025-04-07 19:27:33 -0400

    [op-geth] Refactor

---
+++ fullnodes/op-geth/flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Op Geth";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    utils = {
+      url = "path:../..";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, flake-utils, nixpkgs, utils }:
+  flake-utils.lib.eachDefaultSystem (system:
+  let
+    pkgs = import nixpkgs { inherit system; };
+
+    makeImageConfig = package: {
+      config = {
+        Entrypoint = [ "${package}/bin/geth" ];
+        User = "1000:1000";
+      };
+    };
+
+    generatedFlake = utils.lib.${system}.makeFlake {
+      inherit makeImageConfig;
+      name = "op-geth";
+      version = "1.101503.1";
+    };
+  in {
+    packages = generatedFlake.packages;
+    apps = generatedFlake.apps // utils.lib.${system}.loginApp;
+  });
+}

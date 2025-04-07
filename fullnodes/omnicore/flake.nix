006aa414646e71f8572e701d3bb743170be2db22
Alex Wied <alexwied@coinmetrics.io>
2025-04-07 19:21:17 -0400

    [omnicore] Refactor

---
+++ fullnodes/omnicore/flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Omnicore";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    utils = {
+      url = "path:../..";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, flake-utils, nixpkgs, utils }:
+  flake-utils.lib.eachDefaultSystem (system:
+  let
+    pkgs = import nixpkgs { inherit system; };
+
+    makeImageConfig = package: {
+      config = {
+        Entrypoint = [ "${package}/bin/omnicored" ];
+        User = "1000:1000";
+      };
+    };
+
+    generatedFlake = utils.lib.${system}.makeFlake {
+      inherit makeImageConfig;
+      name = "omnicore";
+      version = "0.12.0.1";
+    };
+  in {
+    packages = generatedFlake.packages;
+    apps = generatedFlake.apps // utils.lib.${system}.loginApp;
+  });
+}

dc904027de6a288e6f90070fb2233dd590f35527
Alex Wied <alexwied@coinmetrics.io>
2025-04-07 20:22:49 -0400

    [polkadot] Refactor

---
+++ fullnodes/polkadot/flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Polkadot";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    utils = {
+      url = "path:../..";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, flake-utils, nixpkgs, utils }:
+  flake-utils.lib.eachDefaultSystem (system:
+  let
+    pkgs = import nixpkgs { inherit system; };
+
+    makeImageConfig = package: {
+      config = {
+        Entrypoint = [ "${package}/bin/polkadot" ];
+        User = "1000:1000";
+      };
+    };
+
+    generatedFlake = utils.lib.${system}.makeFlake {
+      inherit makeImageConfig;
+      name = "polkadot";
+      version = "1.15.1";
+    };
+  in {
+    packages = generatedFlake.packages;
+    apps = generatedFlake.apps // utils.lib.${system}.loginApp;
+  });
+}

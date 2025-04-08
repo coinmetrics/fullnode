ba5038a88c919081ed7d9670b283ab0efea5b978
Alex Wied <alexwied@coinmetrics.io>
2025-04-07 20:14:46 -0400

    [pivx] Refactor

---
+++ fullnodes/pivx/flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "PIVX";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    utils = {
+      url = "path:../..";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, flake-utils, nixpkgs, utils }:
+  flake-utils.lib.eachDefaultSystem (system:
+  let
+    pkgs = import nixpkgs { inherit system; };
+
+    makeImageConfig = package: {
+      config = {
+        Entrypoint = [ "${package}/bin/pivxd" "-paramsdir=${package}/share/pivx" ];
+        User = "1000:1000";
+      };
+    };
+
+    generatedFlake = utils.lib.${system}.makeFlake {
+      inherit makeImageConfig;
+      name = "pivx";
+      version = "5.6.1";
+    };
+  in {
+    packages = generatedFlake.packages;
+    apps = generatedFlake.apps // utils.lib.${system}.loginApp;
+  });
+}

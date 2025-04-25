0afbecc81cd852f716c68d13cf2bfb379782ec0e
Andrew Schmid <andy@coinmetrics.io>
2025-04-25 08:04:45 -0500

    Resolve FULLNODE-852 "Zcash node v6 2 0"
    Update zcash to v6.2.0
    
    Closes FULLNODE-852

--- fullnodes/zcash/flake.nix
+++ fullnodes/zcash/flake.nix
@@ -32,7 +32,7 @@
     generatedFlake = with pkgs; utils.lib.${system}.makeFlake {
       inherit makeImageConfig;
       name = "zcash";
-      version = "6.1.0";
+      version = "6.2.0";
       vars = {
         inherit (darwin.apple_sdk.frameworks) Security;
         boost = boost183;

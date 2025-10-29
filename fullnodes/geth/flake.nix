8825b5ccf0d82f1ff3eb96432bf79c0ffaa45295
Saulo Ricci <saulo@coinmetrics.io>
2025-10-29 14:14:53 -0700

    FULLNODE-1262: Updating geth to v1.16.5
    Closes FULLNODE-1262

--- fullnodes/geth/flake.nix
+++ fullnodes/geth/flake.nix
@@ -21,7 +21,7 @@
     generatedFlake = utils.lib.${system}.makeFlake {
       inherit makeImageConfig;
       name = "geth";
-      version = "1.16.4";
+      version = "1.16.5";
     };
   in {
     packages = generatedFlake.packages;

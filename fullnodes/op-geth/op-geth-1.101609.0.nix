a9c8d288e093cce66198021300641e263c2177d4
Andrew Schmid <andy@coinmetrics.io>
2026-02-25 13:31:38 -0600

    Resolve FULLNODE-1429 "Ethereum optimism op geth v1 101609 0"
    Upgrade op-geth to v1.101609.0
    
    Closes FULLNODE-1429

--- fullnodes/op-geth/op-geth-1.101609.0.nix
+++ fullnodes/op-geth/op-geth-1.101609.0.nix
@@ -1,13 +1,13 @@
 { buildGoModule, fetchFromGitHub }:
 buildGoModule rec {
   pname = "op-geth";
-  version = "1.101608.0";
+  version = "1.101609.0";
 
   src = fetchFromGitHub {
     owner = "ethereum-optimism";
     repo = "op-geth";
     rev = "v${version}";
-    hash = "sha256-YtTUwQOQrCoNXIB6j7Ihp6oLWNeU+uJSdgw+D2IPnPo=";
+    hash = "sha256-lJJjUDcKavqJHRKKhSUK+TcrPYTlGi50MHas8KtzzoE=";
   };
 
   vendorHash = "sha256-XS9Z+dG/8P9CLkgBKWmhPORTWarufufHJ7zKwRhpaao=";
